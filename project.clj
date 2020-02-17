68fe8a375646574ac126efb51c29c8bdeac4076b
Elena Slavcheva <elena.slavcheva@fundingcircle.com>
2020-02-17 10:10:35 +0200

    Get app only token and test post request

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/data.json "0.2.7"]
+                 [org.clojure/data.codec "0.1.1"]
+                 [clj-http "3.10.0"]
+                 [clj-oauth "1.5.5"]]
   :main ^:skip-aot twitter-clj-app.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/twitter_clj_app/core.clj
+++ src/twitter_clj_app/core.clj
@@ -1,7 +1,58 @@
 (ns twitter-clj-app.core
-  (:gen-class))
+  (:require [clojure.data.codec.base64 :as b64]
+            [clj-http.client :as client]
+            [oauth.signature :as oas]))
+
+(def api-endpoint
+  "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=ElenaSlavcheva&count=2")
+
+(def token-endpoint
+  "https://api.twitter.com/oauth2/token")
+
+(def consumer-key
+  "consumer-key")
+
+(def consumer-secret
+  "consumer-secret")
+
+(def app-only-token
+  "token")
+
+(defn- encode-app-only-key
+  "Given a consumer-key and consumer-secret, concatenates and Base64
+  encodes them so that they can be submitted to Twitter in exchange
+  for an application-only token."
+  [consumer-key consumer-secret]
+  (let [concat-keys (str (oas/url-encode consumer-key) ":" (oas/url-encode consumer-secret))
+        base64-bytes (b64/encode (.getBytes concat-keys))]
+    (String. ^bytes base64-bytes "UTF-8")))
+
+(defn call-twitter!
+  []
+  (client/get api-endpoint
+              {:async? true
+               :headers {"Authorization" (str "Bearer " app-only-token)}}
+            ;; respond callback
+              (fn [response] (println "response is:" response))
+            ;; raise callback
+              (fn [exception] (println "exception message is: " (.toString exception)))))
+
+(defn get-token!
+  []
+  (let [auth-string (str "Basic " (encode-app-only-key consumer-key consumer-secret))
+        content-type "application/x-www-form-urlencoded;charset=UTF-8"]
+    (client/post token-endpoint
+                {:async? true
+                 :headers {"Authorization" auth-string
+                           "Content-Type" content-type}
+                 :body "grant_type=client_credentials"
+                 :redirect-strategy :none}
+            ;; respond callback
+                (fn [response] (println "response is:" response))
+            ;; raise callback
+                (fn [exception] (println "exception message is: " (.toString exception))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [& _args]
+  (println "try getting token"))
