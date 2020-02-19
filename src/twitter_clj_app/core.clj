59385c24d2aee3ac9b7790281c5ec72d403a4f22
Nikolay Kotsev <nikolay.kotzev@fundingcircle.com>
2020-02-19 10:44:43 +0200

    Use env, :as and :query-params

--- src/twitter_clj_app/core.clj
+++ src/twitter_clj_app/core.clj
@@ -1,22 +1,27 @@
 (ns twitter-clj-app.core
   (:require [clojure.data.codec.base64 :as b64]
             [clj-http.client :as client]
-            [oauth.signature :as oas]))
+            [oauth.signature :as oas]
+            [environ.core :refer [env]]))
 
-(def api-endpoint
-  "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=ElenaSlavcheva&count=2")
+(defn env-or-throw
+  "Loads `variable-name` from ENV or raises IllegalArgumentException."
+  [variable-name]
+  (or
+    (env variable-name)
+    (throw (IllegalArgumentException. (str variable-name " wasn't found in the environment")))))
 
-(def token-endpoint
-  "https://api.twitter.com/oauth2/token")
+(def skip-ssl? true)
 
-(def consumer-key
-  "consumer-key")
+(def api-url "https://api.twitter.com/1.1/")
 
-(def consumer-secret
-  "consumer-secret")
+(def api-endpoint (str api-url "statuses/user_timeline.json"))
 
-(def app-only-token
-  "token")
+(def token-endpoint "https://api.twitter.com/oauth2/token")
+
+(def consumer-key (env-or-throw :twitter-api-key))
+
+(def consumer-secret (env-or-throw :twitter-api-secret))
 
 (defn- encode-app-only-key
   "Given a consumer-key and consumer-secret, concatenates and Base64
@@ -27,32 +32,33 @@
         base64-bytes (b64/encode (.getBytes concat-keys))]
     (String. ^bytes base64-bytes "UTF-8")))
 
-(defn call-twitter!
-  []
-  (client/get api-endpoint
-              {:async? true
-               :headers {"Authorization" (str "Bearer " app-only-token)}}
-            ;; respond callback
-              (fn [response] (println "response is:" response))
-            ;; raise callback
-              (fn [exception] (println "exception message is: " (.toString exception)))))
-
 (defn get-token!
   []
   (let [auth-string (str "Basic " (encode-app-only-key consumer-key consumer-secret))
-        content-type "application/x-www-form-urlencoded;charset=UTF-8"]
-    (client/post token-endpoint
-                {:async? true
-                 :headers {"Authorization" auth-string
-                           "Content-Type" content-type}
-                 :body "grant_type=client_credentials"
-                 :redirect-strategy :none}
-            ;; respond callback
-                (fn [response] (println "response is:" response))
-            ;; raise callback
-                (fn [exception] (println "exception message is: " (.toString exception))))))
+        content-type "application/x-www-form-urlencoded;charset=UTF-8"
+        request (client/post token-endpoint
+                             {:insecure skip-ssl?
+                              :headers {"Authorization" auth-string
+                                        "Content-Type" content-type}
+                              :body "grant_type=client_credentials"
+                              :as :json-strict
+                              :redirect-strategy :none})]
+    (get-in request [:body :access_token])))
+
+(def app-only-token (future (get-token!)))
+
+(defn call-twitter!
+  []
+  (client/get api-endpoint
+              {:insecure? skip-ssl?
+               :async false
+               :as :json-strict
+               :query-params {"screen_name" "ElenaSlavcheva"
+                              "count" 2}
+               :headers {"Authorization" (str "Bearer " @app-only-token)}}))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& _args]
-  (println "try getting token"))
+  (println ((call-twitter!) :body))
+  (shutdown-agents))
